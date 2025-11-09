d99c42a2432dfe1a741bdc2ea6bc083765ec51af
Lars <lars.dehairs@gmail.com>
2025-11-09 21:34:55 +0100

    Change base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/your-repo-name/', // 👈 add this line
+    base: '/Radio-Toolbox/', // 👈 add this line
 
     server: {
       port: 3000,
